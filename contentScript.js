ca7886653e8ea9d0265c7b2eeb1eb888c59adacc
杢 <c361@postal.jpn.org>
2021-07-18 22:28:33 +0900

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -233,7 +233,7 @@ getLogs = event => {
 	}
 	
 	console.log(event.detail ? data : JSON.stringify(data, undefined, '\t')),
-	console.log(`${d.getFullYear()}-${(d.getMonth() + 1 +'').padStart(2,'0')}${(d.getDate()+'').padStart(2,'0')}-${(d.getHours()+'').padStart(2,'0')}${(d.getMinutes()+'').padStart(2,'0')}.html`);
+	console.log(`${d.getFullYear()}-${(d.getMonth() + 1 +'').padStart(2,'0')}${(d.getDate()+'').padStart(2,'0')}-${(d.getHours()+'').padStart(2,'0')}${(d.getMinutes()+'').padStart(2,'0')}.json`);
 	
 },
 
